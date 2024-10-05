b7eff222af18ef2cb42ec65a6f0fc09138effb61
likejean <popachs@yahoo.com>
2024-10-05 09:51:14 -0700

    Added express middleware: urlencoded

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
+let {people} = require('./data');
 
-app.get('/',(req, res)=>{
-    res.end("Home");
+//middleware: exposes a directory or a file to 
+//a particular URL so its contents can be publicly accessed (thru inspection in browser)
+app.use(express.static('./public'));
+
+//middleware: moduleparses request URL-encoded payloads
+app.use(express.urlencoded({extended: false}));
+
+
+app.get('/api/people',(req, res)=>{
+    res.status(200).json({success: true, data: people})
+})
+
+app.post('/login',(req, res)=>{
+    console.log(req.body)
+    res.end('POST');
 })
 
 app.listen(5000, ()=>{
