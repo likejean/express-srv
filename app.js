1964e3a94ea283b8c73558a0a9299c766bd87c5c
likejean <popachs@yahoo.com>
2024-10-04 20:05:44 -0700

    added fake data.js file

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-
+app.get('/',(req, res)=>{
+    res.end("Home");
+})
 
 app.listen(5000, ()=>{
     console.log('Server is listening on port 5000....')
