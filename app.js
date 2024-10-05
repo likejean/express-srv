8ece75d954d0a1d67994110efe56c1c31b77a8cc
likejean <popachs@yahoo.com>
2024-10-05 11:14:38 -0700

    added express middlware to parse json-type requests

--- app.js
+++ app.js
@@ -6,13 +6,29 @@ let {people} = require('./data');
 //a particular URL so its contents can be publicly accessed (thru inspection in browser)
 app.use(express.static('./public'));
 
-//middleware: moduleparses request URL-encoded payloads
+//middleware: module parses request URL-encoded payloads
 app.use(express.urlencoded({extended: false}));
 
+//middleware: module parses incoming requests with JSON payloads and is based on body-parser
+app.use(express.json());
 
 app.get('/api/people',(req, res)=>{
     res.status(200).json({success: true, data: people})
 })
+
+app.post('/api/people',(req, res)=>{
+    const {name} = req.body;
+    if(!name) {
+        return res.
+        status(400).
+        json({success: false, msg: 'please, provide your name!!!!'});
+    }
+    
+    res.status(201).json({success: true, person: name});
+})
+
+
+
 
 app.post('/login',(req, res)=>{
     const {name} = req.body;
