add24b67493ed8b42311974500c4dc5769e81271
likejean <popachs@yahoo.com>
2024-10-06 12:12:23 -0700

    syntax error fix

--- api/routes/sensors.js
+++ api/routes/sensors.js
@@ -13,7 +13,7 @@ router.get('/', (req, res, next) => {
         .then(docs => {
             res.status(200).json({
                 count: docs.length,
-                sensors: docs.map(doc => {
+                sensors: docs.map(doc => {                    
                     return {
                         EID: doc.EID,
                         type: doc.type,    
@@ -29,16 +29,17 @@ router.get('/', (req, res, next) => {
                         comment: doc.comment,
                         units: doc.units,
                         manufacturer: doc.manufacturer,
-                        createdAt:req.doc.createdAt,
+                        createdAt: doc.createdAt,
                         request: {
                             type: 'GET'
                         }
-                    };
+                    };                    
                 })
             });
         })
         .catch(err => {
             res.status(500).json({
+                comment: 'something is wrong...',
                 error: err
             });
         });
