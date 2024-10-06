1a9c7b1792115dee78318e254b4aa357e68d888d
likejean <popachs@yahoo.com>
2024-10-05 21:27:38 -0700

    Added folder tree for 'api': 'routes', 'models', 'auth'

--- app.js
+++ app.js
@@ -21,32 +21,5 @@ app.use(express.urlencoded({extended: false}));
 //middleware: module parses incoming requests with JSON payloads and is based on body-parser
 app.use(express.json());
 
-app.get('/api/people',(req, res)=>{
-    res.status(200).json({success: true, data: people})
-})
-
-app.post('/api/people',(req, res)=>{
-    const {name} = req.body;
-    if(!name) {
-        return res.
-        status(400).
-        json({success: false, msg: 'please, provide your name!!!!'});
-    }
-    
-    res.status(201).json({success: true, person: name});
-})
-
-
-
-
-app.post('/login',(req, res)=>{
-    const {name} = req.body;
-    if(name){
-        return res.status(200).send(`Welcome, ${name}`)
-    }
-    res.status(403).send("please, provide crendentials");
-})
-
-
 
 module.exports = app;
