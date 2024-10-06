ba317857bd04e7d390d947f013db77a45f80f893
likejean <popachs@yahoo.com>
2024-10-06 11:44:50 -0700

    updated GET/POST API endpoints for sensor collection

--- api/models/sensor.js
+++ api/models/sensor.js
@@ -9,6 +9,7 @@ const sensorSchema = mongoose.Schema({
     expirationDate: { type: Date, default: Date.now, required: true },
     createdAt: { type: Date, default: Date.now, required: true },
     calibrationExtended: { type: Boolean, required: true },
+    calibratedBy: { type: String, required: false }, 
     maxCalibrationExtension: { type: String, required: false },
     location: { type: String, required: false },
     description: { type: String, required: false },
