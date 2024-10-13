5d7478bd5b984c28685d869644b2e61d4339ff30
likejean <popachs@yahoo.com>
2024-10-12 21:48:24 -0700

    Added boostrap modal to index.html file, updated sensor card html content

--- public/js/displaySettings.js
+++ public/js/displaySettings.js
@@ -10,10 +10,12 @@ function showSensorDetailsAndSettings(
      
     //Initialize html element variables
     var card = document.getElementById('sensor-details');
+    var descriptionHeader = document.querySelector('.sensor-description');
+    const descriptionText = document.createTextNode(description);   
     var tableRow;
 
-    let lastCalDate = moment(lastCalibrationDateDate);
-    let dueCalDate = moment(dueCalibrationDate);
+    let lastCalDate = moment.utc(lastCalibrationDateDate);
+    let dueCalDate = moment.utc(dueCalibrationDate);
     console.log(lastCalDate.format('dddd MM/DD/YYYY'),lastCalDate.unix())
     console.log(dueCalDate.format('dddd MM/DD/YYYY'),dueCalDate.unix());
     const unixTimestamp = moment().unix();
@@ -27,7 +29,8 @@ function showSensorDetailsAndSettings(
             rowObj[`${key}`]='inactive';
             tableRow = document.getElementById(key);
             tableRow.classList.remove("table-active");
-            card.style.visibility = 'hidden';
+            card.style.visibility = 'hidden';    
+            removeAllChildNodes(descriptionHeader); 
         }         
       } 
       
@@ -37,7 +40,9 @@ function showSensorDetailsAndSettings(
         rowObj[`row${index}`] = 'active';
         tableRow = document.getElementById(`row${index}`);    
         tableRow.classList.add("table-active"); 
-        card.style.visibility = 'visible';
+        card.style.visibility = 'visible';          
+        descriptionHeader.appendChild(descriptionText);        
+        
     //deactive a current table row [index] and hide its info card 
     //(only if it was activated during previous click event)
     }else{
@@ -45,6 +50,14 @@ function showSensorDetailsAndSettings(
         tableRow = document.getElementById(`row${index}`);
         tableRow.classList.remove("table-active"); 
         card.style.visibility = 'hidden';
+        removeAllChildNodes(descriptionHeader);
     }  
+
+    //Function helper for removing all children from parent html node
+    function removeAllChildNodes(parent) {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
+    }
        
 }
