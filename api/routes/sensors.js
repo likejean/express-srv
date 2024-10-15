4b13ab39b62a8b61e258c84280da082e2715c668
Sergey Popach <40197774+likejean@users.noreply.github.com>
2024-10-15 00:42:51 +0000

    check

--- api/routes/sensors.js
+++ api/routes/sensors.js
@@ -228,7 +228,8 @@ router.post('/', (req, res, next) => {
                     calibrationExtended,
                     maxCalibrationExtension,
                     calibrationRangePercent
-                );                
+                );
+                console.log(calProcedure)                
                 cals[i].sensors.push(sensor._id);
                 sensor.calibrations.push(calProcedure);                                
             }
