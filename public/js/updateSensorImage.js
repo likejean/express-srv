8e13e703fd7864df59ad7806e6061201b8eb797c
Sergey Popach <40197774+likejean@users.noreply.github.com>
2024-10-18 20:32:43 +0000

    Added another sensor to database

--- public/js/updateSensorImage.js
+++ public/js/updateSensorImage.js
@@ -3,7 +3,8 @@ function updateSensorImage (EID){
         EID47: "load_transducer_EID47.jpg",
         EID498: "load_transducer_EID498.jpg",
         EID1798: "load_transducer_EID1798.jpg",
-        EID1722: "load_transducer_EID1722.jpg"
+        EID1722: "load_transducer_EID1722.jpg",
+        EID555: "extensometer_EID555.jpg"
     };
     
     var img = document.getElementById('sensor-image');
