a6c843047d23b63e0a3a0c348aecb69c2a0f872c
Sergey Popach <40197774+likejean@users.noreply.github.com>
2024-10-28 23:15:42 +0000

    Modified updateSensorImage function for edge case: undefined (sensor image is not found in collection)

--- public/js/collections/updateSensorImages.js
+++ public/js/collections/updateSensorImages.js
@@ -1,10 +1,9 @@
 const sensorImageCollection = {
-    EID47: "load_transducer_EID47.jpg",
-    EID498: "load_transducer_EID498.jpg",
-    EID1798: "load_transducer_EID1798.jpg",
-    EID1722: "load_transducer_EID1722.jpg",
-    EID555: "extensometer_EID555.jpg",
-    EID199: "deflectometer_EID199.jpg"
+  EID47: "load_transducer_EID47.jpg",
+  EID498: "load_transducer_EID498.jpg",
+  EID1798: "load_transducer_EID1798.jpg",
+  EID1722: "load_transducer_EID1722.jpg",
+  EID555: "extensometer_EID555.jpg",
+  EID199: "deflectometer_EID199.jpg",
+  //EID3005: "load_transducer_EID3005.jpg",
 };
-
-
--- public/js/utils/updateSensorImage.js
+++ public/js/utils/updateSensorImage.js
@@ -1,4 +1,9 @@
-function updateSensorImage (EID){       
-    var img = document.getElementById('sensor-image');
-    img.src=`./img/${sensorImageCollection[EID]}`;
+//this function is being called in showSensorInfoCard.js
+function updateSensorImage(EID) {
+  var img = document.getElementById("sensor-image");
+
+  //if image is not listed in collection, get a default image
+  if (!sensorImageCollection[EID]) img.src = `./img/calibration.jpg`;
+  //else display associated iamge
+  else img.src = `./img/${sensorImageCollection[EID]}`;
 }
