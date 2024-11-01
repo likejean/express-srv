e0dfd58dc01f0f6e6f5abc211cf4659771cc7278
likejean <popachs@yahoo.com>
2024-10-31 21:13:41 -0700

    Created User Mongoose Model

---
+++ api/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const userLevels = [
+    'Admin',
+    'User',
+    'Quest'
+];
+
+const userSchema = new Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    password: { type: String, required: true },
+	username: {
+		type: String,
+		required: true,
+		unique: true
+	},	
+	level: {
+		type: String,
+		required: true,
+		enum: userLevels
+	}
+});
+
+module.exports = mongoose.model('User', userSchema);
