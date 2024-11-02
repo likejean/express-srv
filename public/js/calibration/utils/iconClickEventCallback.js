e1d537012619b69ba8102438357ca809045e1760
likejean <popachs@yahoo.com>
2024-11-01 19:03:00 -0700

    added comments to iconClickEventCallback.js file

--- public/js/calibration/utils/iconClickEventCallback.js
+++ public/js/calibration/utils/iconClickEventCallback.js
@@ -1,34 +1,39 @@
 ///Callback function for ONCLICK eventListener in editCalibrationEvent.html file...
 ////.......  icon.addEventListener('click', iconClickEventCallback);
 function iconClickEventCallback(event) {
-	const wrapperName = trimBySubstring(event.target.id, "Icon").concat(
-		"Wrapper"
-	);
+	
+	const wrapperName = trimBySubstring(event.target.id, "Icon").concat("Wrapper");
 	const wrapper = document.getElementById(wrapperName);
-	let input = document.createElement(
-		_calfactory.inputWrappers[wrapperName].tag
-	);
-	let icon = document.getElementById(
-		_calfactory.inputWrappers[wrapperName].name.concat("Icon")
-	);
+	let input = document.createElement(_calfactory.inputWrappers[wrapperName].tag);
+	let icon = document.getElementById(_calfactory.inputWrappers[wrapperName].name.concat("Icon"));
 	const patchButton = document.getElementById("patch-cal-record");
 
 	//creates toggle functionality for icons in Calibration Summary Card
 	if (!_calfactory.inputWrappers[wrapperName].status) {
 		icon.classList.replace("fa-ellipsis-vertical", "fa-file-pen");
-		_calfactory.inputWrappers[wrapperName].childNodes = Array.from(
-		wrapper.childNodes
-		);
+		//reserve the previous html child nodes in the calibration factory
+		_calfactory.inputWrappers[wrapperName].childNodes = Array.from(wrapper.childNodes);
+		//initialize value of the edit input
+		input.value = wrapper.textContent;
+
+		//remove all child nodes of the text wrapper
 		removeAllChildNodes(wrapper);
+		//activate edit mode for targeted text wrapper
 		_calfactory.inputWrappers[wrapperName].status = true;
+
+		//construct & append the edit input to empty wrapper
 		input.classList.add(_calfactory.inputWrappers[wrapperName].class);
 		input.setAttribute("name", _calfactory.inputWrappers[wrapperName].name);
-		input.setAttribute("type", _calfactory.inputWrappers[wrapperName].type);
-		input.setAttribute("style", "border-color:blue;");
+		input.setAttribute("type", _calfactory.inputWrappers[wrapperName].type);		
+		input.setAttribute("style", "border-color:blue;");		
 		wrapper.appendChild(input);
+
+		//attach event listener to edit input
 		input.addEventListener("input", inputChangeValueCallback);
 		patchButton.disabled = !_calfactory.isPatchButtonActive();
 	} else {
+
+		//reverse changes to original wrapper state and calibration factory
 		input.removeEventListener("input", inputChangeValueCallback);
 		document.getElementById("inputErrorMessage").textContent = "";
 		removeAllChildNodes(wrapper);
