655c039b56717bbb340e309445b09411eff62b84
likejean <popachs@yahoo.com>
2024-11-10 12:24:47 -0800

    Added visibility styling to main bar anchors

--- public/js/main/displayLoginUserStatus.js
+++ public/js/main/displayLoginUserStatus.js
@@ -1,14 +1,25 @@
 function displayLoginUserStatus() {
 	if(window.localStorage.getItem("token") !== null) {
-		console.log(window.localStorage.getItem("token"))
 		document.getElementById("logout-anchor").style.visibility = "visible";
-		document.getElementById("login-anchor").style.visibility = "hidden";
+		//document.getElementById("login-anchor").style.visibility = "hidden";
+		hideAndShrink(document.getElementById("login-anchor"));
+		
 	}
 	else {
 		console.log(window.localStorage.getItem("token"))
 		document.getElementById("login-anchor").style.visibility = "visible";
-		document.getElementById("logout-anchor").style.visibility = "hidden";
+		//document.getElementById("logout-anchor").style.visibility = "hidden";
+		hideAndShrink(document.getElementById("logout-anchor"));		
 	}
+}
+
+
+function hideAndShrink(element) {
+	element.style.visibility = "hidden";
+	element.style.height = "0";
+	element.style.width = "0";
+	element.style.padding = "0";
+	element.style.margin = "0";
 }
 
 displayLoginUserStatus();
