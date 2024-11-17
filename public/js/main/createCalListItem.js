c60c50cfb1ff295766d0aae1dfbce37eedb48771
likejean <popachs@yahoo.com>
2024-11-16 20:24:21 -0800

    Revised calibration certificate list

--- public/js/main/createCalListItem.js
+++ public/js/main/createCalListItem.js
@@ -1,11 +1,38 @@
 //this function creates list of all calibrations associated with specified sensor
+
+function generateToastHtmlItem (id, link, certificateName, data) {
+	return `<div id="${id}" class="toast show mx-2 my-1" role="alert" aria-live="assertive" aria-atomic="true">
+		<div class="toast-header">
+			<a href="${link}"><i class="fas fa-2x fa-certificate mx-1"></i></a>
+			<strong id="certificate-name" class="me-auto">${certificateName}</strong>
+			<small>11 mins ago</small>
+			<button type="button" class="btn-close" data-bs-dismiss="toast"></button>			
+		</div>
+		<div class="toast-body">
+			Hello, world! This is a toast message.
+		</div>
+	</div>`
+}
+
 function createCalibrationListItem (data, list, calName, calLocation) {     //called in showSensorInfoCard.js
     //Get a current time in unix format
     const unixTimestamp = moment().unix();     
 
-    for (let i=0; i < data.length; i++){
+    for (let i = 0; i < data.length; i++){
+
+		let toastItem = document.createElement("div");
+		
+		toastItem.innerHTML = generateToastHtmlItem (
+			`${data[i]._id}`,
+			`./html/editCalibrationEvent.html?id=${data[i]._id}`, 
+			`${data[i].calibrationName.replace(/{/g, "").replace(/}/g, "")}`, 
+			data
+		);
+		
+		document.getElementById("calibration-certificates").appendChild(toastItem);
+
 		let listItem = document.createElement("li");
-		let badgeItem = document.createElement("a");        
+		let calLinkItem = document.createElement("a");        
 
 		let lastCalDate = moment.utc(data[i].lastCalibrationDate);
 		let dueCalDate = moment.utc(data[i].dueCalibrationDate);
@@ -13,23 +40,25 @@ function createCalibrationListItem (data, list, calName, calLocation) {     //ca
 
 
 		//add url with query params to navigate Calibration Event Card for edits
-		badgeItem.setAttribute('id', `${data[i]._id}`)
-		badgeItem.classList.add('calibration-record-link');
-		badgeItem.href=`./html/editCalibrationEvent.html?id=${data[i]._id}`;       
+		calLinkItem.setAttribute('id', `${data[i]._id}`)
+		calLinkItem.href=`./html/editCalibrationEvent.html?id=${data[i]._id}`;       
 
 		//add class attributes to calName badge and calInfo list item
-		badgeItem.classList.add('list-group-item');
+		calLinkItem.classList.add('list-group-item');		
 		listItem.classList.add('list-group-item');
-		badgeItem.classList.add('d-flex');
+		calLinkItem.classList.add('d-flex');
 		listItem.classList.add('d-flex');
+		calLinkItem.classList.add('justify-content-center');
 
-		if(data[i].calibrationExtended) badgeItem.innerHTML = `<span id="extensionStatus" class="badge badge-success">EXT</span>`;
+		calLinkItem.innerHTML = `<i class="fas fa-certificate"></i>`;
+
+		//if(data[i].calibrationExtended) calLinkItem.innerHTML = `<span id="extensionStatus" class="badge badge-success">EXT</span>`;
 		
 		//CAL INFO LIST/////////////////////////////////////////////////////////
 		//add css styles to calName badge and calInfo list item
 		listItem.style.color = dueCalDate.unix() > unixTimestamp ? "black" : "red";       
-		badgeItem.style.backgroundColor = dueCalDate.unix() > unixTimestamp ? "green" : "red";        
-		badgeItem.style.color = "white";  
+		calLinkItem.style.backgroundColor = dueCalDate.unix() > unixTimestamp ? "green" : "red";        
+		calLinkItem.style.color = "white";  
 
 		const calNameText = document.createTextNode(`
 			${data[i].calibrationName.replace(/{/g, "").replace(/}/g, "")}`
@@ -42,10 +71,10 @@ function createCalibrationListItem (data, list, calName, calLocation) {     //ca
 			\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0Extended? ${extenstionStatus?"YES":"NO"}
 			\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0Expired? ${dueCalDate.unix()>unixTimestamp?"NO":"YES"}`
 		);	   
-		badgeItem.appendChild(calNameText); 
+		//calLinkItem.appendChild(calNameText); 
 		listItem.appendChild(calInfoText); 
 		list.appendChild(listItem); 
-		calName.appendChild(badgeItem);      
+		calName.appendChild(calLinkItem);      
 		
 	}    
 }
