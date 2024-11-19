07f082a1e19d30c4575ac2ac6b9fa7148cce7be2
likejean <popachs@yahoo.com>
2024-11-18 20:26:18 -0800

    Added more fields for user profile

--- public/js/user/fetchUserRecordById.js
+++ public/js/user/fetchUserRecordById.js
@@ -1,6 +1,12 @@
 const username = document.getElementById("sensor-name");
 const avatarImg = document.getElementById("avatar-image");
-const userFullName = document.getElementById("user-fullname");
+const userFullName = document.getElementById("user-profile-fullname");
+const userEmail = document.getElementById("user-profile-email");
+const userNickname = document.getElementById("user-profile-nickname");
+const userAboutYourself = document.getElementById("user-profile-about-yourself");
+const userAge = document.getElementById("user-profile-age");
+const userLevel= document.getElementById("user-profile-level");
+
 
 const fetchUserRecordById = async () => {
     
@@ -17,6 +23,11 @@ const fetchUserRecordById = async () => {
 			//render decoded image
 			avatarImg.src = `data:image/*; base64, ${toBase64(imgArray)}`;
 			userFullName.innerHTML = result.data.user.firstname + " " + result.data.user.lastname;
+			userEmail.innerHTML = result.data.user.email;
+			userNickname.innerHTML = result.data.user.username;
+			userAge.innerHTML = result.data.user.age;
+			userAboutYourself.innerHTML = result.data.user.aboutYourself;
+			userLevel.innerHTML = result.data.user.level;
 		})
 		.catch((error) => {
 			//display error message if data fetch failure occurs or any other internal error detected
