2d852ab25d0be39db616b4189c334d61ffb42578
likejean <popachs@yahoo.com>
2024-11-21 20:18:20 -0800

    cont'd working on filterSensorsByDescription() function

--- public/js/utils/filterSensorsByDescription.js
+++ public/js/utils/filterSensorsByDescription.js
@@ -4,23 +4,28 @@ function filterSensorsByDescription() {
 	sensorDescriptionTableHeader.removeAttribute("onclick");
 	sensorDescriptionTableHeader.removeAttribute("scope");
 	sensorDescriptionTableHeader.removeChild(textNode);
-	sensorDescriptionTableHeader.style.padding = "0";
+	sensorDescriptionTableHeader.style.padding = "1px";
 	
 	console.log(_store.getSensorDescriptionList());
 
 	const select = document.createElement('select');
 
-	const option = document.createElement('option');
-	option.value = "All";
-	option.text = "All";
-	select.appendChild(option);
+	select.style.width = "100%";
+	select.style.height = "40px";
+	select.style.background = "#d3d3d3";
 
-	_store.getSensorDescriptionList().forEach((description) => {
+	["All", ..._store.getSensorDescriptionList(), "Clear Filter"].forEach((description) => {
 		const option = document.createElement('option');
+		if (description === "Clear Filter") {
+			option.style.color = "red";
+			option.style.fontWeight = "bold";
+		}
 		option.value = description;
 		option.text = description;
 		select.appendChild(option);
 	})
+
+	
 
 	sensorDescriptionTableHeader.appendChild(select);
 
