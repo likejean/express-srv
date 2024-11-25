b84250b7f15397dd43c434cf2492ea1b5fc6f299
Sergey Popach <40197774+likejean@users.noreply.github.com>
2024-11-25 17:12:41 +0000

    Added extensometer image

--- public/js/collections/updateSensorImages.js
+++ public/js/collections/updateSensorImages.js
@@ -4,6 +4,7 @@ const sensorImageCollection = {
 	EID1798: "load_transducer_EID1798.jpg",
 	EID1722: "load_transducer_EID1722.jpg",
 	EID555: "extensometer_EID555.jpg",
+	EID604: "extensometer_EID604.jpg",
 	EID199: "deflectometer_EID199.jpg",
 	EID3005: "load_transducer_EID3005.jpg",
 	EID1663: "load_transducer_EID1663.jpg",
@@ -11,4 +12,5 @@ const sensorImageCollection = {
 	EID1280: "load_transducer_EID1280.jpg",
 	EID756: "cod_clipon-gage_EID756.jpg",
 	EID757: "cod_clipon-gage_EID757.jpg",
+	EID807: "extensometer_EID807.jpg"
 };
