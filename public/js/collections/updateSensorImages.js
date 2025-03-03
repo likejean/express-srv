fc21e3a1236c9446d5375096d1f2b99b6787dc83
Sergey Popach <40197774+likejean@users.noreply.github.com>
2025-03-03 17:01:01 +0000

    Added new sensor images

--- public/js/collections/updateSensorImages.js
+++ public/js/collections/updateSensorImages.js
@@ -1,5 +1,6 @@
 const sensorImageCollection = {
 	EID47: "load_transducer_EID47.jpg",
+	EID47: "load_transducer_EID50.jpg",
 	EID498: "load_transducer_EID498.jpg",
 	EID1798: "load_transducer_EID1798.jpg",
 	EID1722: "load_transducer_EID1722.jpg",
