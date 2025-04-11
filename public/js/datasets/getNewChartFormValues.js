33be70b4bec71f15d0b694ae2ef521ae8ed9d234
Sergey Popach <40197774+likejean@users.noreply.github.com>
2025-04-11 16:54:26 +0000

    Continued troubleshooting...

--- public/js/datasets/getNewChartFormValues.js
+++ public/js/datasets/getNewChartFormValues.js
@@ -224,15 +224,15 @@ function submitChartDatasets() {
 	//user input keys that must be cleaned out
 	const keysToRemove = ["sensorError", "calibratorOutput", "errorType", "datasetSize", "seriesDescription", "seriesLabel", "calibrationName"];
 	
-	debugger;
 	
-	for (const [key, value] of formData.entries()) {		
+	for (const [key, value] of formData.entries()) {	
 		if (key === "datasetStartAt" || key === "datasetEndAt" || key === "errorLimit") newChartDatasetPostData[key] = Number(value);
 		else newChartDatasetPostData[key] = value;
 	}
 	
 	//adds new key-pairs to POST request object
 	newChartDatasetPostData["sensorId"] = _store.activeSensorCard._id;
+	newChartDatasetPostData["errorLimit"] = Number(newChartDatasetPostData["errorLimit"]);
 	newChartDatasetPostData["sensorDescription"] =`${_store.activeSensorCard.description} ${ _store.activeSensorCard.EID}`;
 	
 	//stores array of sensor error output dataset(s). An array element contains the following object:
