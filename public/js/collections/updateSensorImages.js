cb5b9b9c9946962bdea9cdd4fd525b409eb0a7ff
Sergey Popach <40197774+likejean@users.noreply.github.com>
2025-05-21 16:30:10 +0000

    Fixed EID number for image

--- public/js/collections/updateSensorImages.js
+++ public/js/collections/updateSensorImages.js
@@ -1,7 +1,7 @@
 const sensorImageCollection = {
 	EID47: "load_transducer_EID47.jpg",
 	EID50: "load_transducer_EID50.jpg",
-	EID51: "load_transducer_EID51.jpg",
+	EID46: "load_transducer_EID46.jpg",
 	EID498: "load_transducer_EID498.jpg",
 	EID1798: "load_transducer_EID1798.jpg",
 	EID1722: "load_transducer_EID1722.jpg",
