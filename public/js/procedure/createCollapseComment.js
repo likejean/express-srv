97bf5c365b8eb824bbfc2ba79f713310ac2c7546
likejean <popachs@yahoo.com>
2025-06-21 10:55:01 -0700

    Added icons to procedures details table columns

--- public/js/procedure/createCollapseComment.js
+++ public/js/procedure/createCollapseComment.js
@@ -1,40 +1,65 @@
+// Function to create a collapsible comment section in a table cell
+// Usage: createCollapseComment(tag, idx, comment)
+// Example: createCollapseComment('td', 1, 'This is a comment')
 
 function createCollapseComment (tag, idx, comment) {    
-    //add any html attributes (if any)
-
+    
+	// create a table cell element with the specified tag
+	// and add the classes for responsive visibility
     let cell = document.createElement(tag);
+	["d-none", "d-lg-table-cell"].forEach(classItem => cell.classList.add(classItem));
 
+	//create the open and close buttons and the comment text element
 	let openBtn = document.createElement("button");
     let closeBtn = document.createElement("button");
     let commentTextElem = document.createElement("div");
   
+	//add classes to the buttons and comment text element
     ["btn", "btn-primary"].forEach(classItem => openBtn.classList.add(classItem));
     ["btn", "btn-danger", "m-3"].forEach(classItem => closeBtn.classList.add(classItem));
     ["card", "card-body"].forEach(classItem => commentTextElem.classList.add(classItem));
 
+	//set attributes and text for the buttons and comment text element
     openBtn.setAttribute("id", `open-button-${idx}`);
-    openBtn.innerText = "View details";
+	
+	// Create the icon element
+	const openIcon = document.createElement('i');	
+	openIcon.classList.add('fa-solid', 'fa-folder-closed'); // Font Awesome folder closed icon	
+	
+	// Append the icon to the button
+	openBtn.appendChild(openIcon);
     openBtn.style.display = 'block';
-    closeBtn.setAttribute("id", `close-button-${idx}`);
-    closeBtn.innerText = "Close details";
+
+	const closeIcon = document.createElement('i');
+	closeIcon.classList.add('fa-solid', 'fa-rectangle-xmark'); // Font Awesome rectangular xmark icon
+	closeBtn.appendChild(closeIcon);
+
+	// Set the text for the buttons
+    closeBtn.setAttribute("id", `close-button-${idx}`);	
     closeBtn.style.display = 'none';
     commentTextElem.setAttribute("id", `comment-text-${idx}`);
     commentTextElem.innerText = comment;
     commentTextElem.style.display = 'none';
 
-     cell.appendChild(openBtn);
+	// Append the buttons and comment text element to the cell
+    cell.appendChild(openBtn);
     [openBtn, commentTextElem, closeBtn].forEach(elem => cell.appendChild(elem));
 
 
+	// Add event listeners for the open and close buttons
+	// When the open button is clicked, it hides itself, shows the close button, and displays the comment text
+	// When the close button is clicked, it hides itself, shows the open button, and hides the comment text
     openBtn.onclick = function (e) {
-        let id = getLastPart(e.target.id, "-");
-        document.getElementById(`open-button-${id}`).style.display = 'none';
-        document.getElementById(`close-button-${id}`).style.display = 'block';
-        document.getElementById(`comment-text-${id}`).style.display = 'block';
+		let id = getLastPart(e.currentTarget.id, "-");
+		document.getElementById(`open-button-${id}`).style.display = 'none';
+		document.getElementById(`close-button-${id}`).style.display = 'block';
+		document.getElementById(`comment-text-${id}`).style.display = 'block';
+	
+        
     }
 
     closeBtn.onclick = function (e) {
-        let id = getLastPart(e.target.id, "-");
+        let id = getLastPart(e.currentTarget.id, "-");
         document.getElementById(`close-button-${id}`).style.display = 'none';
         document.getElementById(`open-button-${id}`).style.display = 'block';
         document.getElementById(`comment-text-${id}`).style.display = 'none';
@@ -42,6 +67,7 @@ function createCollapseComment (tag, idx, comment) {
     return cell;
 }
 
+// Helper function to get the last part of a string after a specific character
 function getLastPart(str, char) {
     const parts = str.split(char);
     return parts.pop();
--- public/js/procedure/createProceduresTable.js
+++ public/js/procedure/createProceduresTable.js
@@ -7,14 +7,20 @@ function createProceduresTable() {
 	for(let i = 0; i < data.length; i++){
 		// adds the row of cells to the end of the table body
         let row = document.createElement("tr");
+
+		//append rows to the table body
 		row.setAttribute("id", `row${i + 1}`);
+
+		// Create and append each cell to the row
+		// The first cell is a header cell with the row number
+		// The rest of the cells are data cells with the procedure information
 		row.appendChild(createTableCell("th",`${i + 1}`, [],  [{attribute:"scope", value:"row"}])); //# column cell
-		row.appendChild(createTableCell("td",`${data[i].procedureName}`, [],  [])); //Procedure Name cell
-		row.appendChild(createTableCell("td",`${data[i].description}`, ["d-none", "d-sm-table-cell"],  [])); //Description cell
-		row.appendChild(createTableCell("td",`${data[i].calibratorModel}`, ["d-none", "d-sm-table-cell"],  [])); //Calibrator Model cell
-		row.appendChild(createTableCell("td",`${data[i].manufacturer}`, ["d-none", "d-lg-table-cell"],  [])); //Calibrator Manufacturer cell
-		row.appendChild(createTableCell("td",`${data[i].measurementQuantity}`, ["d-none", "d-md-table-cell"],  [])); //Measurement Quantity cell
-		row.appendChild(createTableCell("td",`${data[i].units}`, ["d-none", "d-md-table-cell"],  [])); //Units cell		
+		row.appendChild(createTableCell("td",`${data[i].procedureName}`, [],  [{attribute:"id", value: `name_${i + 1}`}])); //Procedure Name cell
+		row.appendChild(createTableCell("td",`${data[i].description}`, ["d-none", "d-lg-table-cell"],  [{attribute:"id", value: `description_${i + 1}`}])); //Description cell
+		row.appendChild(createTableCell("td",`${data[i].calibratorModel}`, ["d-none", "d-sm-table-cell"],  [{attribute:"id", value: `model_${i + 1}`}])); //Calibrator Model cell
+		row.appendChild(createTableCell("td",`${data[i].manufacturer}`, ["d-none", "d-lg-table-cell"],  [{attribute:"id", value: `manufacturer_${i + 1}`}])); //Calibrator Manufacturer cell
+		row.appendChild(createTableCell("td",`${data[i].measurementQuantity}`, ["d-none", "d-md-table-cell"],  [{attribute:"id", value: `quantity_${i + 1}`}])); //Measurement Quantity cell
+		row.appendChild(createTableCell("td",`${data[i].units}`, ["d-none", "d-md-table-cell"],  [{attribute:"id", value: `units_${i + 1}`}])); //Units cell		
 		row.appendChild(createDropDownList("td", data[i].calibrations, [])); //Calibrations drop-down list
 		row.appendChild(createCollapseComment("td", i + 1, data[i].comment)); //Comments Manufacturer cell
 		row.appendChild(createTrashIcon(`${i + 1}`, `${data[i]._id}`, [], data[i].calibrations.length !== 0))
