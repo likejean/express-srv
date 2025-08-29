6844f235cabc1a2dd80bf268876ab996da70039c
Sergey Popach <40197774+likejean@users.noreply.github.com>
2025-08-29 23:35:10 +0000

    updated sensor image object

--- public/js/collections/updateSensorImages.js
+++ public/js/collections/updateSensorImages.js
@@ -18,6 +18,7 @@ const sensorImageCollection = {
 	EID161: "load_transducer_EID161.jpg",
 	EID1280: "load_transducer_EID1280.jpg",
 	EID1477: "load_transducer_EID1477.jpg",
+	EID3007: "load_transducer_EID3007.jpg",
 	EID756: "cod_clipon-gage_EID756.jpg",
 	EID757: "cod_clipon-gage_EID757.jpg",
 	EID807: "extensometer_EID807.jpg",
