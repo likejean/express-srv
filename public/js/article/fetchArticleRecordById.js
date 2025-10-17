d7d2e86feef19339d959835f3d1bd36e2309e3b8
likejean <popachs@yahoo.com>
2025-10-17 08:44:18 -0700

    Repeat

--- public/js/article/fetchArticleRecordById.js
+++ public/js/article/fetchArticleRecordById.js
@@ -4,17 +4,23 @@ const calibratorModel = document.getElementById("calibrator-model");
 const calibratorManufacturer = document.getElementById("calibrator-manufacturer");
 const procedureName = document.getElementById("procedure-name");
 
-
+//function to fetch article record by id
+//id is obtained from query string
 const fetchArticleRecordById = async () => {
+
     //obtain query string by id
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     let id = urlParams.get("id");
 
+	//fetch procedure record by id
+	//populate html elements with fetched data
+	//axios get request to fetch procedure record by id
 	await axios
 	.get(`../api/procedures/${id}`)
 	.then((result) => {	
 		console.log("Fetched procedure record:", result);
+		
 		//populate html elements with data
 		//populate fetched procedure data in Procedure Description Card html elements
 		procedureDescription.innerText = result.data.sensor.description;
@@ -29,5 +35,6 @@ const fetchArticleRecordById = async () => {
 	});
 }
 
+//call fetchArticleRecordById function on page load
 fetchArticleRecordById();
 
