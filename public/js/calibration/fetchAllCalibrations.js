36e11aaf52677507c2068e33b3fa88358dcccff4
likejean <popachs@yahoo.com>
2025-11-09 11:12:45 -0800

    viewCalibrations.html page created

---
+++ public/js/calibration/fetchAllCalibrations.js
@@ -0,0 +1,25 @@
+//data storage object to hold all fetched calibration procedure calibrations data
+const _store = new dataStorage();   
+
+//function to fetch all calibrations
+const fetchAllCalibrations = async () => {
+
+	// fetch all data using POST API endpoints: calibrations
+	await axios.get("/api/calibrations", {
+			headers: getRequestHeaders()
+		})
+		.then((result) => {
+			_store.calibrations = result.data.payload;	
+			console.log(_store.calibrations);
+		})
+		.then(() => {			
+		})
+		.catch((error) => {
+			//display error message if data fetch failure occurs or any other internal error detected
+			console.log(error);
+		}
+	);
+}
+
+//call fetchAllCalibrations.js function
+fetchAllCalibrations();
