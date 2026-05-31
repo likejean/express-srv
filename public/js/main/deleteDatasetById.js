a687077e944d450ac5f937308a4392d5fdc209c7
likejean <popachs@yahoo.com>
2026-05-31 08:44:18 -0700

    Added comments to deleteDatasetByid

--- public/js/main/deleteDatasetById.js
+++ public/js/main/deleteDatasetById.js
@@ -3,14 +3,23 @@ function openDatasetWarningModal () {
     modalTitle.innerText = `${_store.activeSensorCard.description}: ${_store.activeSensorCard.EID}`;
 }
 
+//this function is called when the user clicks the "Delete Dataset" button on the dataset details card
+//it sends a DELETE request to the server with the dataset ID and sensor information to delete the dataset record from the database
 function deleteDatasetRecord() {
+
+	// Get dataset ID and sensor information from the store
 	const id = _store.activeDatasetChart._id;
+
+	// Prepare data to send in the DELETE request body
     const data = {
         sensorEID: _store.activeSensorCard.EID,
         sensorDescription:  _store.activeSensorCard.description
     };
+	//
 	modal.hide();
     
+	// Send DELETE request to the server with dataset ID and sensor information
+	// The server will use this information to identify and delete the correct dataset record from the database
     axios
         .delete(`../api/datasets/${id}`, { data })
         .then((response) => {
